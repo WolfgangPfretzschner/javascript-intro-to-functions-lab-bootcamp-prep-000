32c84cf4d1b968729835c69384acd0c78b7a0674
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-03-28 07:53:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  
+  if (string === 'hello'){
+    return 'I can\'t hear you!'
+  }if else(string === 'HELLO'){
+    return ''
+  }
 }
